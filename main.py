494d722a965bbd4012d63288737573bc006f21e5
Giftcard <apiluarmor@gmail.com>
2026-05-03 08:40:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.author.bot and message.author.id != bot.user.id:  # Responde a otros bots
-        respuesta = "@everyone\nhttps://www.roblox.com/share?code=7c1aaa90013002498dfbeaf12e72651d&type=Server"
-        await message.channel.send(respuesta)
+    # Evita que el bot responda a sus propios mensajes
+    if message.author.id == bot.user.id:
+        return
     
-    # Para que también responda si un usuario escribe (opcional)
-    # elif not message.author.bot:
-    #     await message.channel.send("@everyone\nhttps://www.roblox.com/share?code=7c1aaa90013002498dfbeaf12e72651d&type=Server")
+    # Responde a TODOS los mensajes (usuarios y bots)
+    respuesta = "@everyone\nhttps://www.roblox.com/share?code=7c1aaa90013002498dfbeaf12e72651d&type=Server"
+    
+    await message.channel.send(respuesta)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
